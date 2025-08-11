ae7d13444cc69af976eea7bd8cad63a956232c31
Kingsley Nwosu <accesskings@yahoo.co.uk>
2025-08-11 22:40:36 +0100

    Debug flake8

--- src/train.py
+++ src/train.py
@@ -2,6 +2,7 @@ import pickle
 from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
 
+
 def main():
     X, y = load_iris(return_X_y=True)
     clf = RandomForestClassifier()
@@ -9,6 +10,7 @@ def main():
     with open('src/model/model.pkl', 'wb') as f:
         pickle.dump(clf, f)
     print("Model trained and saved to src/model/model.pkl")
+
 
 if __name__ == "__main__":
     main()
